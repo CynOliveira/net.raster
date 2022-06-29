e532215d2f947645172f3fe6784321064562ef8d
Cynthia Valéria Oliveira <cynthia.valeria.oliveira@gmail.com>
2022-06-29 09:14:03 -0300

    updating and deleting previous raster_function for now

--- R/interact_strength_metrics.R
+++ R/interact_strength_metrics.R
@@ -1,11 +1,36 @@
 ## Interaction strenght (b)
 
+#' This function calculates interaction strength for each specie in the network.
+#'
+#' @param x an adjacency matrix, which have one row and one column for each species in the bipartite network. The elements of the matrix can be any number, but in case of binary networks they will be either 0 or 1. A matrix element of 1 (or greater) signals that the respective column species (plants) and row species (frugivores) interact in the network.
+#' @param ... arguments to be passed to internal functions
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' #' net1 <- matrix(c(0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0), nrow = 5, ncol = 5, dimnames = list(c("B1", "B2", "B3", "B4", "B5"), c("P1", "P2", "P3", "P4", "P5")))
+#' b(net1)
+#'
 b <- function(x, ...){
   bij <- x/rowSums(x)
   return(bij)
 }
 
-## Interaction strength asymmetry (AS)
+#' Interaction strength asymmetry (AS)
+#'
+#'This function calculates interaction strength asymmetry for each specie in the network.
+#'
+#' @param x an adjacency matrix, which have one row and one column for each species in the bipartite network. The elements of the matrix can be any number, but in case of binary networks they will be either 0 or 1. A matrix element of 1 (or greater) signals that the respective column species (plants) and row species (frugivores) interact in the network.
+#' @param ... arguments to be passed to internal functions
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' net1 <- matrix(c(0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0), nrow = 5, ncol = 5, dimnames = list(c("B1", "B2", "B3", "B4", "B5"), c("P1", "P2", "P3", "P4", "P5")))
+#' b(net1)
+#'
 AS <- function(x, ...){
   bij <- x/rowSums(x)
   bji <- x/colSums(x)
@@ -15,3 +40,4 @@ AS <- function(x, ...){
   return(asij)
 }
 
+document()
